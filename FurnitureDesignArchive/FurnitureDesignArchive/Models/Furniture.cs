6d022934afc6a7307692b516bdd49e3046325fee
mthomps4 <matthew.thompson.a@gmail.com>
2016-10-15 21:00:09 -0400

    Sorted out my Database/Model Woes -- Next Task -- Update Views Accordingly.

--- FurnitureDesignArchive/FurnitureDesignArchive/Models/Furniture.cs
+++ FurnitureDesignArchive/FurnitureDesignArchive/Models/Furniture.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace FurnitureDesignArchive.Models
 {
     public class Furniture
     {
+       public enum IntensityLevel
+        {
+            Beginner,
+            Intermediate,
+            Hard
+        }
+
+        public enum FurnitureType
+        {
+            Tables,
+            Seating,
+            Containers,
+            Misc
+        }
+
         public int FurnitureID { get; set; }
-        public string FurnitureName { get; set; } 
-        public string FurnitureType { get; set; } //<list> Table, Chair, Container, Misc
+        public string FurnitureName { get; set; }
         public string FurnitureImg { get; set; } //URL string 
+        public IntensityLevel buildLevel { get; set; }
+        public FurnitureType furnitureType { get; set; }
         public double BoardFootEst { get; set; } //12.5 BF
         public string PartList { get; set; } //Explode string "Part: ..." 
-        public int TimeLevel { get; set; } //1-5
-        public string AdditionalNotes { get; set; } 
+        public string AdditionalNotes { get; set; }
+        public bool CompletedBefore { get; set; }
     }
 
     public class FurnitureContext : DbContext
     {
         public DbSet<Furniture> FurniturePieces { get; set; }
     }
+
+
 }
