445dda4cce7c351e01de8d92caba607c3297c2e6
mthomps4 <matthew.thompson.a@gmail.com>
2016-10-31 22:17:55 -0400

    Creating Seed Data With Custom Initializer.

--- FurnitureDesignArchive/FurnitureDesignArchive/Models/FurnitureDBContext.cs
+++ FurnitureDesignArchive/FurnitureDesignArchive/Models/FurnitureDBContext.cs
@@ -9,26 +9,20 @@ namespace FurnitureDesignArchive.Models
     // example entity framework create database if not exists seed data
     // http://www.entityframeworktutorial.net/code-first/database-initialization-strategy-in-code-first.aspx
     //https://www.asp.net/mvc/overview/getting-started/getting-started-with-ef-using-mvc/creating-an-entity-framework-data-model-for-an-asp-net-mvc-application
+
     public class FurnitureDBContext : DbContext
     {
 
-        public class FurnitureDBInitializer : CreateDatabaseIfNotExists<FurnitureDBContext>
+        public FurnitureDBContext(): base("Furniture Database") //Connection String 
         {
-            protected override void Seed(FurnitureDBContext context)
-            {
-                base.Seed(context);
-            }
+            Database.SetInitializer<FurnitureDBContext>(new FurnitureDBInitializer());
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseIfModelChanges<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseAlways<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new SchoolDBInitializer());
         }
-
-            //CreateDatabaseIfNotExists<FurnitureDBContext>());
-            //DropCreateDatabaseIfModelChanges<SchoolDBContext>());
-
-
-            //DropCreateDatabaseAlways<SchoolDBContext>());
-       
 
         public DbSet<Furniture> FurniturePieces { get; set; }
         public DbSet<FurniturePart> FurnitureParts { get; set; }
-    }
 
-}
+    }//DBContext
+}//Namespace
